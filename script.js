1b2a7b98723d8f6e245163b0aa0097c3dbd59623
miishhub <msibi8708@gmail.com>
2025-05-29 10:07:47 +0530

    commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// named function 
+// function hello(){
+//     console.log("hello world")
+// }
+// hello()
+
+const greet = (name) => {
+    console.log("hello "+name);
+
+} 
+greet("rizwzan")
+greet("greece")
+greet("kerala")
